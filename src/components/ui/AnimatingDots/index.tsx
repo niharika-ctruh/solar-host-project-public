f3232393eccaff80b314db0424202c8caecf96d1
Niharika <147478186+niharika-ctruh@users.noreply.github.com>
2025-12-03 12:57:23 +0530

    AnimatingDots layout

--- src/components/ui/AnimatingDots/index.tsx
+++ src/components/ui/AnimatingDots/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 const AnimatingDots: FC<{ color: string }> = ({ color }) => {
   return (
-    <div className={`flex h-full space-x-3`}>
+    <div className="flex h-full space-x-3 items-center">
       <div
         className={`animate-pulse-scale h-[7px] w-[7px] rounded-full ${color}`}
       ></div>
